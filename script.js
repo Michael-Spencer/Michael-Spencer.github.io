3d8286c60ac2dbf731dbcf480cc11ec4fa91de41
Michael-Spencer <mspencer1@live.co.uk>
2024-05-12 19:54:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation');
+const closeBtn = document.querySelector('.close-btn');
 
 menuToggle.addEventListener('click', () => {
   navigation.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navigation.classList.remove('active');
 });
